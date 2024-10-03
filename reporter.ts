6ff45c8f9c8072dd40311fd4a15020651e2e697e
arroyave <luis.arroyave@talent.com>
2024-10-03 15:57:49 -0500

    add personalized report

--- reporter.ts
+++ reporter.ts
@@ -1,23 +1,63 @@
-import type {
-  FullConfig, FullResult, Reporter, Suite, TestCase, TestResult
-} from '@playwright/test/reporter';
+import { Reporter, TestCase, TestResult, FullResult, FullConfig, Suite } from "@playwright/test/reporter";
+import moment from "moment";
 
 class MyReporter implements Reporter {
-  onBegin(config: FullConfig, suite: Suite) {
-    console.log(`Starting the run with ${suite.allTests().length} tests`);
-  }
+  introMessage = "";
+  failsMessage = "";
+  passed = 0;
+  failed = 0;
+  skipped = 0;
 
-  onTestBegin(test: TestCase, result: TestResult) {
-    console.log(`Starting test ${test.title}`);
+  onBegin(config: FullConfig, suite: Suite) {
+    this.introMessage = `- Test run started at ${moment().format("MMMM Do YYYY, h:mm:ss a")} 
+- Number tests cases to run: ${suite.allTests().length}`;
   }
 
   onTestEnd(test: TestCase, result: TestResult) {
-    console.log(`Finished test ${test.title}: ${result.status}`);
+    switch (result.status) {
+      case "failed":
+      case "timedOut":
+        this.addFailMessage(`❌ Test ${test.title} failed\n>${result.error?.message}`);
+        this.failed++;
+        break;
+      case "skipped":
+        this.addFailMessage(`⚠️ Test ${test.title} skipped`);
+        this.skipped++;
+        break;
+      case "passed":
+        console.log(`✅ Test ${test.title} ${result.status}`);
+        this.passed++;
+        break;
+    }
   }
 
-  onEnd(result: FullResult) {
-    console.log(`Finished the run: ${result.status}`);
+  async onEnd(result: FullResult) {
+    const message = await this.buildMessage(result);
+    // Implement your Logic to send this message whenever you want.
+    // e.i. Send to Slack using incoming webhooks with a simple POST request
   }
+
+  private addFailMessage(message: string) {
+    this.failsMessage += `\n${message}`;
+  }
+
+  private async buildMessage(result: FullResult) {
+     const duration = moment.duration(result.duration, "milliseconds");
+     const minutes = Math.floor(duration.asMinutes());
+     const seconds = duration.seconds();
+
+    const resultMarkdownMessage = `\n\nTest run results\n-------------------\n${this.introMessage}\n-------------------\n
+Summary:\n
+- ⌛ Duration  of test run: ${minutes} minutes and ${seconds} seconds
+- 📦 Tests results:
+- ✅ ${this.passed}
+- ❌ ${this.failed}
+- ⏩ ${this.skipped}\n
+${this.failsMessage ? `Tests Failed ❌\n${this.failsMessage}` : "👍 All tests passed successfully!"}\n
+To see the full report, please visit our CI/CD piplene with reporter.`;
+    console.log(resultMarkdownMessage);
+  }
+  
 }
 
 export default MyReporter;
