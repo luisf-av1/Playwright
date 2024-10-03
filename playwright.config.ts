751c2907ee49e02c2d99bf892bc4168ed1dc274b
arroyave <luis.arroyave@talent.com>
2024-10-03 14:50:57 -0500

    add built-in report

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,9 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['./reporter.ts'],
-    ['html']
+    ['html'],
+    ["junit", { outputFile: "playwright-report/pw/results-junit.xml", open: "never" }],
+    ["html", { outputFolder: "playwright-report/pw/html/", open: "never" }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
