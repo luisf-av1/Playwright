2fed15d4dfff16920909b34ddc1826c88fa193ab
arroyave <luis.arroyave@talent.com>
2024-10-03 13:22:49 -0500

    add pw config

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: "off",
+    screenshot: "off",
+    headless: true,
+    viewport: {width:1280, height: 720}
   },
 
   /* Configure projects for major browsers */
