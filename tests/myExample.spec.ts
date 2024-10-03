b6b264b6e68c8077c08243e5299171612856c3b2
arroyave <luis.arroyave@talent.com>
2024-10-03 09:32:48 -0500

    add simple click and select element by text test

--- tests/myExample.spec.ts
+++ tests/myExample.spec.ts
@@ -8,4 +8,11 @@ test("First PW Test", async({page})=>{
     await expect(title).toHaveText('Playwright');
 
     // await page.pause();
-})
+});
+
+test("Simple click test", async ({page})=>{
+    await page.goto("https://the-internet.herokuapp.com/");
+    await page.locator('text=Add/Remove Elements').click(); //select element by text
+    await page.locator('text=Add Element').click();
+
+});
