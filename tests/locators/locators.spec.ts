de8f8b529c507b630a8258016699fab4fd723e37
arroyave <luis.arroyave@talent.com>
2024-10-03 19:34:44 -0500

    add xpath locator example

--- tests/locators/locators.spec.ts
+++ tests/locators/locators.spec.ts
@@ -2,8 +2,13 @@ const {test, expect } = require('@playwright/test');
 
 test("Testign selectors", async({page})=>{
     await page.goto("https://demoqa.com/text-box");
-    await page.locator("#userName").type("Test username");
-    await page.locator("[placeholder='name@example.com']").type("test@test.com");
+
+    //xpath selector
+    const title = page.locator("xpath=//h1[contain(text(),'Check B')]")
+    await page.locator("//input[@id='userName']").type("Test username"); 
+
+    //css selector
+    await page.locator("[placeholder='name@example.com']").type("test@test.com"); 
     await page.locator("#currentAddress").type("This is the current address");
     await page.locator("#permanentAddress").type("This is a permanent address");
     await page.locator("button:has-text('Submit')").click();
