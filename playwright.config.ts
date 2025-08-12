084daca3c83986e535a3e2fc8f12369f7c0c2309
Luis AV <109425175+luisf-av1@users.noreply.github.com>
2025-08-12 10:59:01 -0500

    add json report

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,15 +18,16 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 0 : 0,
+  retries: process.env.CI ?  1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['./reporter.ts'],
-     // ["list", { printSteps: true }],
-    ["junit", { outputFile: "playwright-report/pw/results-junit.xml", open: "never" }],
-    ["html", { outputFolder: "playwright-report/pw/html/", open: "never" }],
+    ['html', {  outputFile: 'dashboard/html/test-results.xml' }, { open: 'never' }],
+    ['list'],
+    ['dot'],
+    ['junit', {  outputFile: 'dashboard/test-results.xml' }],
+    ['json', {  outputFile: 'dashboard/test-results.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
