0705817990f1f0595decb9f999db206dd2e2d683
Luis AV <109425175+luisf-av1@users.noreply.github.com>
2025-08-12 11:07:31 -0500

    fix cofig

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html', {  outputFile: 'dashboard/html/test-results.html' }, { open: 'never' }],
+    ['html', {  outputFile: 'dashboard/html/test-results.html', open: 'never' }],
     ['list'],
     ['dot'],
     ['junit', {  outputFile: 'dashboard/test-results.xml' }],
